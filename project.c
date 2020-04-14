27120b860f69bbeef2ee37b8d59f7178f9fcc5d3
Blake Celestian <blakesmudgepot@gmail.com>
2020-04-14 19:24:01 -0400

    it returns something

--- project.c
+++ project.c
@@ -12,7 +12,7 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-
+	return 1;
 }
 
 
