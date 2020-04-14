6cce25b36f1b5d12c7224c93c393af8ddb1546d4
Blake Celestian <blakesmudgepot@gmail.com>
2020-04-14 19:47:51 -0400

    wrote instruction_fetch function

--- project.c
+++ project.c
@@ -12,7 +12,11 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-	return 1;
+	if (PC % 4)
+		return 1;
+
+	*instruction = Mem[PC/4];
+	return 0;
 }
 
 
