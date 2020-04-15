ecd8b3e5a862e33641903ac601f0dbceb0b657db
jacob-oquinn <jacob.oquinn374@gmail.com>
2020-04-15 15:36:10 -0400

    Accidently removed this

--- project.c
+++ project.c
@@ -84,7 +84,8 @@ int instruction_decode(unsigned op,struct_controls *controls)
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-	
+	*data1 = Reg[r1];
+	*data2 = Reg[r2];
 }
 
 
